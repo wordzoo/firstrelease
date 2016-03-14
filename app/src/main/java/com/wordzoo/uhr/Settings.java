9f8405ec5ff0438faeb4c3d09c95adf2bf2f7178
Anton Kast <akast@google.com>
2016-03-13 21:47:50 -0700

    Make a functional Settings Activity.

--- app/src/main/java/com/wordzoo/uhr/Settings.java
+++ app/src/main/java/com/wordzoo/uhr/Settings.java
@@ -1,115 +1,104 @@
 package com.wordzoo.uhr;
 
-
 import android.app.Activity;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.EditText;
 
 import java.util.ArrayList;
 
-// Need the following import to get access to the app resources, since this
-// class is in a sub-package.
- //       import io.appium.android.apis.R;
-
-
 public class Settings extends Activity {
-    static final String TAG = "Settings";
 
     private static final String PREFS_NAME
             = "io.appium.android.apis.appwidget.ExampleAppWidgetProvider";
+
     private static final String PREF_PREFIX_KEY = "prefix_";
 
-    int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
-    EditText mAppWidgetPrefix;
+    EditText mEditText;
+    int mAppWidgetId;
 
     public Settings() {
         super();
+        mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
     }
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-
-        // Set the result to CANCELED.  This will cause the widget host to cancel
-        // out of the widget placement if they press the back button.
-        setResult(RESULT_CANCELED);
-
-        // Set the view layout resource to use.
         setContentView(R.layout.settings);
+        mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
 
-        // Find the EditText
-        mAppWidgetPrefix = (EditText)findViewById(R.id.appwidget_prefix);
-
-        // Bind the action for the save button.
-        findViewById(R.id.save_button).setOnClickListener(mOnClickListener);
-
-        // Find the widget id from the intent.
+        // Set the widget id from the intent.  (This never happens.)
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
         if (extras != null) {
             mAppWidgetId = extras.getInt(
                     AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+            // If they gave us an intent without the widget id, just bail.
+            if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+                // Uncomment this once the widget ID is being set.
+//                finish();
+            }
         }
-
-        // If they gave us an intent without the widget id, just bail.
-        if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
-            finish();
-        }
-
-        mAppWidgetPrefix.setText(loadTitlePref(Settings.this, mAppWidgetId));
+        mEditText = (EditText) findViewById(R.id.app_widget_prefix_text);
     }
 
-    View.OnClickListener mOnClickListener = new View.OnClickListener() {
-        public void onClick(View v) {
-            final Context context = Settings.this;
+    // When the Activity starts, initialize the text.
+    @Override
+    public void onResume() {
+        super.onResume();
+        String oldPrefValue = loadTitlePref(this, mAppWidgetId);
+        mEditText.setText(oldPrefValue);
+    }
 
-            // When the button is clicked, save the string in our prefs and return that they
-            // clicked OK.
-            String titlePrefix = mAppWidgetPrefix.getText().toString();
-            saveTitlePref(context, mAppWidgetId, titlePrefix);
+    // When the Activity ends, save the text.
+    @Override
+    public void onPause() {
+        super.onPause();
+        String newPrefValue = mEditText.getText().toString();
+        saveTitlePref(newPrefValue);
+    }
 
-            // Push widget update to surface with newly set prefix
-            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-            ExampleAppWidgetProvider.updateAppWidget(context, appWidgetManager,
-                    mAppWidgetId, titlePrefix);
+    @Override
+    public void onDestroy() {
+        // Push widget update to surface with newly set prefix
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        String newPrefValue = mEditText.getText().toString();
+        ExampleAppWidgetProvider.updateAppWidget(
+                this, appWidgetManager, mAppWidgetId, newPrefValue);
 
-            // Make sure we pass back the original appWidgetId
-            Intent resultValue = new Intent();
-            resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
-            setResult(RESULT_OK, resultValue);
-            finish();
-        }
-    };
+        // Make sure we pass back the original appWidgetId
+        Intent resultValue = new Intent();
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+        setResult(RESULT_OK, resultValue);
+        super.onDestroy();
+    }
 
     // Write the prefix to the SharedPreferences object for this widget
-    static void saveTitlePref(Context context, int appWidgetId, String text) {
-        SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
-        prefs.putString(PREF_PREFIX_KEY + appWidgetId, text);
-        prefs.commit();
+    void saveTitlePref(String text) {
+        SharedPreferences.Editor prefs = getSharedPreferences(PREFS_NAME, 0).edit();
+        prefs.putString(PREF_PREFIX_KEY + mAppWidgetId, text);
+        prefs.apply();
     }
 
     // Read the prefix from the SharedPreferences object for this widget.
     // If there is no preference saved, get the default from a resource
-    static String loadTitlePref(Context context, int appWidgetId) {
+    static String loadTitlePref(Context context, int widgetId) {
         SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, 0);
-        String prefix = prefs.getString(PREF_PREFIX_KEY + appWidgetId, null);
+        String prefix = prefs.getString(PREF_PREFIX_KEY + widgetId, null);
         if (prefix != null) {
             return prefix;
-        } else {
-            return context.getString(R.string.appwidget_prefix_default);
         }
+        return context.getString(R.string.appwidget_prefix_default);
     }
 
     static void deleteTitlePref(Context context, int appWidgetId) {
     }
 
-    static void loadAllTitlePrefs(Context context, ArrayList<Integer> appWidgetIds,
-                                  ArrayList<String> texts) {
+    static void loadAllTitlePrefs(
+            Context context, ArrayList<Integer> appWidgetIds, ArrayList<String> texts) {
     }
 }
-
