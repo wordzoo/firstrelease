fd87febc085841c1440acaa93a446cda8410fb02
tom kast <tomkast@gmail.com>
2016-03-16 20:34:36 +0100

    revert Settings file

--- app/src/main/java/com/wordzoo/uhr/Settings.java
+++ app/src/main/java/com/wordzoo/uhr/Settings.java
@@ -1,27 +1,105 @@
 package com.wordzoo.uhr;
 
 import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 
+public class Settings extends Activity {
 
-public class Settings extends Activity implements View.OnClickListener {
+    private static final String PREFS_NAME
+            = "io.appium.android.apis.appwidget.ExampleAppWidgetProvider";
 
-    private SharedPreferences clockPrefs;
+    private static final String PREF_PREFIX_KEY = "prefix_";
 
-    public void onClick(View v) {
-        //boilerplate
-        //remoteViews.setViewVisibility(pickedOption, View.VISIBLE);
-        SharedPreferences.Editor custClockEdit = clockPrefs.edit();
-        custClockEdit.putInt("coolOption", 1);
-        custClockEdit.commit();
-        finish();
+    TextView mEditText;
+    int mAppWidgetId;
+
+    public Settings() {
+        super();
+        mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
     }
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.settings);
-        clockPrefs = getSharedPreferences("GermanClockPrefs", 0);
+        mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+
+        // Set the widget id from the intent.  (This never happens.)
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            mAppWidgetId = extras.getInt(
+                    AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+            // If they gave us an intent without the widget id, just bail.
+            if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+                // Uncomment this once the widget ID is being set.
+//                finish();
+            }
+        }
+        mEditText = (TextView) findViewById(R.id.textView);
+    }
+
+    // When the Activity starts, initialize the text.
+    @Override
+    public void onResume() {
+        super.onResume();
+        String oldPrefValue = loadTitlePref(this, mAppWidgetId);
+        mEditText.setText(oldPrefValue);
+    }
+
+    // When the Activity ends, save the text.
+    @Override
+    public void onPause() {
+        super.onPause();
+        String newPrefValue = mEditText.getText().toString();
+        saveTitlePref(newPrefValue);
+    }
+
+    @Override
+    public void onDestroy() {
+        // Push widget update to surface with newly set prefix
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        String newPrefValue = mEditText.getText().toString();
+        //GermanClock.update(
+          //      this, appWidgetManager, mAppWidgetId, newPrefValue);
+
+        // Make sure we pass back the original appWidgetId
+        Intent resultValue = new Intent();
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+        setResult(RESULT_OK, resultValue);
+        super.onDestroy();
+    }
+
+    // Write the prefix to the SharedPreferences object for this widget
+    void saveTitlePref(String text) {
+        SharedPreferences.Editor prefs = getSharedPreferences(PREFS_NAME, 0).edit();
+        prefs.putString(PREF_PREFIX_KEY + mAppWidgetId, text);
+        prefs.apply();
+    }
+
+    // Read the prefix from the SharedPreferences object for this widget.
+    // If there is no preference saved, get the default from a resource
+    static String loadTitlePref(Context context, int widgetId) {
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, 0);
+        String prefix = prefs.getString(PREF_PREFIX_KEY + widgetId, null);
+        if (prefix != null) {
+            return prefix;
+        }
+        return "not sure what this should be"; //context.getString(R.string.appwidget_prefix_default);
+    }
+
+    static void deleteTitlePref(Context context, int appWidgetId) {
+    }
+
+    static void loadAllTitlePrefs(
+            Context context, ArrayList<Integer> appWidgetIds, ArrayList<String> texts) {
     }
 }
