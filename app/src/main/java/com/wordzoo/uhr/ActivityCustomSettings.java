d4df4ac7c6b045171bb1db5ec5c01bb8b901c797
tom kast <tomkast@gmail.com>
2016-04-24 21:44:03 +0200

    progres

--- app/src/main/java/com/wordzoo/uhr/ActivityCustomSettings.java
+++ app/src/main/java/com/wordzoo/uhr/ActivityCustomSettings.java
@@ -12,13 +12,20 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RemoteViews;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.germanclock.time.Pieces;
 import com.germanclock.time.Settings;
+import com.germanclock.words.HalberMinute;
+import com.germanclock.words.TimeInWords;
+
+import java.util.Date;
 
 public class ActivityCustomSettings extends Activity implements OnClickListener, AdapterView.OnItemSelectedListener {
 
@@ -30,8 +37,12 @@ public class ActivityCustomSettings extends Activity implements OnClickListener,
     private RadioButton defButton;
     private Button done;
     private Button cancel;
+    private Spinner spinner;
 
-    Context context;
+    private Context context;
+    private Settings settings;
+
+
 
     public void setupSpinners(int id1, int id2) {
         Spinner spinner = (Spinner) findViewById(id1);
@@ -41,6 +52,8 @@ public class ActivityCustomSettings extends Activity implements OnClickListener,
         spinner.setAdapter(adapter);
 
     }
+
+
     public void onCreate(Bundle savedInstanceState) {
         /*
         Boolean default = this.getResources().getBoolean(R.integer.officallong);
@@ -51,6 +64,8 @@ public class ActivityCustomSettings extends Activity implements OnClickListener,
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.customsettings);
+
+        this.settings = loadSettings();
 
         addDoneButtonListener();
         setupSpinners(R.id.dreiviertel, R.array.settings_dreiviertel);
@@ -63,13 +78,20 @@ public class ActivityCustomSettings extends Activity implements OnClickListener,
         setupSpinners(R.id.nacht, R.array.settings_nacht);
         setupSpinners(R.id.frueh, R.array.settings_frueh);
         //user prefs
-        clockPrefs = getSharedPreferences("CustomClockPrefs", 0);
 
         this.context = this;
 
-
     }
-    private Spinner spinner;
+
+    public Settings getSettings() {
+        return settings;
+    }
+
+    public void setSettings(Settings settings) {
+        this.settings = settings;
+    }
+
+
 
     public void addDoneButtonListener() {
 
@@ -84,7 +106,6 @@ public class ActivityCustomSettings extends Activity implements OnClickListener,
                 finish();
 
             }
-            
 
 
         });
@@ -93,59 +114,15 @@ public class ActivityCustomSettings extends Activity implements OnClickListener,
 
     }
 
-    public Settings loadSettings(String def) {
+    public Settings loadSettings() {
         Settings s = new Settings();
-
+        s.setUmgangssprachlich(Boolean.TRUE);
+        s.setUmgangminute(Settings.Umgangminute.minuteword);
 
         Toast.makeText(ActivityCustomSettings.this,
                 "lookn for..." + def + "...", Toast.LENGTH_SHORT).show();
 
-        // Is the button now checked?
-        if(def.equals("offiziell lang")) {
 
-            s.setEsist(Boolean.TRUE);
-            s.setUhr(Boolean.TRUE);
-            s.setMinute(Boolean.TRUE);
-
-        }
-        else if(def.equals("offiziell kurz")) {
-
-            //
-
-        }
-        else if(def.equals("umgangssprachlich")) {
-
-            s.setUmgangssprachlich(Boolean.TRUE);
-            s.setUmgangminute(Settings.Umgangminute.minuteword);
-            s.setHalber(Boolean.TRUE);
-            s.setHalberRange(10);
-            s.setKurznach(Boolean.TRUE);
-            s.setViertel(Settings.Viertel.viertelacht);
-            s.setZehnvorhalb(Boolean.TRUE);
-            s.setFuenfvorhalb(Boolean.TRUE);
-            s.setKurzvorhalb(Boolean.TRUE);
-            s.setHalb(Settings.Halb.halb);
-            s.setKurznachhalb(Boolean.TRUE);
-            s.setFuenfnachhalb(Boolean.TRUE);
-            s.setZehnnachhalb(Boolean.TRUE);
-            s.setFuenfvordreiviertelacht(Boolean.TRUE);
-            s.setKurzvordreiviertelacht(Boolean.TRUE);
-            s.setDreiviertel(Settings.Dreiviertel.dreiviertelacht);
-            s.setKurznachdreiviertelacht(Boolean.TRUE);
-            s.setFuenfnachdreiviertelacht(Boolean.TRUE);
-            s.setKurzvor(Boolean.TRUE);
-            s.setAmabend(Boolean.TRUE);
-            s.setAmmorgen(Boolean.TRUE);
-            s.setInderfrueh(Boolean.TRUE);
-            s.setAmnachmittag(Boolean.TRUE);
-            s.setAmvormittag(Boolean.TRUE);
-
-            s.setEsist(Boolean.TRUE);
-
-        }
-        else if(def.equals(Settings.Default.custom)) {
-            //comming soon.....
-        }
 
         return s;
 
@@ -186,145 +163,117 @@ public class ActivityCustomSettings extends Activity implements OnClickListener,
         // Is the view now checked?
         boolean checked = ((CheckBox) view).isChecked();
 
+        String time = "08:45";
+        TextView testclock = (TextView)findViewById(R.id.testclock);
+        TextView preview = (TextView)findViewById(R.id.preview);
+
+
         // Check which checkbox was clicked
         switch(view.getId()) {
             case R.id.uhr:
                 if (checked)
-                    ;
-                // Put some meat on the sandwich
+                    getSettings().setUhr(Boolean.TRUE);
                 else
-                    ;
-                // Remove the meat
+                    getSettings().setUhr(Boolean.FALSE);
                 break;
             case R.id.esist:
                 if (checked)
-                    ;
-                // Cheese me
+                    getSettings().setEsist(Boolean.TRUE);
                 else
-                    ;
-                // I'm lactose intolerant
+                    getSettings().setEsist(Boolean.FALSE);
                 break;
-            // TODO: Veggie sandwich
+            case R.id.minute:
+                if (checked)
+                    getSettings().setMinute(Boolean.TRUE);
+                else
+                    getSettings().setMinute(Boolean.FALSE);
+                break;
+            case R.id.lederhosen_minutes:
+                time = "8:48";
+                if (checked) {
+                    getSettings().setUmgangminute(Settings.Umgangminute.minutebar);
+                    toggleAllKurz(Boolean.FALSE);
+                    toggleHalber(Boolean.FALSE);
+                }
+                else {
+                    getSettings().setUmgangminute(Settings.Umgangminute.minuteword);
+                    toggleAllKurz(Boolean.TRUE);
+                    toggleHalber(Boolean.TRUE);
+                }
+                break;
+            case R.id.kurz_vor:
+                time = "08:57";
+                if (checked)
+                    getSettings().setKurzvor(Boolean.TRUE);
+                else
+                    getSettings().setKurzvor(Boolean.FALSE);
+                break;
+            case R.id.kurz_nach:
+                time = "09:02";
+                if (checked)
+                    getSettings().setKurznach(Boolean.TRUE);
+                else
+                    getSettings().setKurznach(Boolean.FALSE);
+                break;
+            case R.id.halb:
+                time = "09:30";
+                if (checked)
+                    getSettings().setHalb(Settings.Halb.halb);
+                else
+                    getSettings().setHalb(Settings.Halb.dreissig);
+                break;
+            case R.id.halber:
+                time = "09:29";
+                if (checked) {
+                    getSettings().setHalber(Boolean.TRUE);
+                    getSettings().setHalberRange(5);
+                }
+                else {
+                    getSettings().setHalber(Boolean.FALSE);
+                }
+                break;
+
         }
+        preview.setText("Preview: " + time);
+        testclock.setText(new TimeInWords(this.context).getTimeAsSentance(new Pieces(time), getSettings()));
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public void toggleAllKurz(Boolean t){
+        CheckBox def = (CheckBox) findViewById(R.id.kurz_nach);
+        def.setEnabled(t);
+        def = (CheckBox) findViewById(R.id.kurz_vor);
+        def.setEnabled(t);
+        def = (CheckBox) findViewById(R.id.kurz_nach_viertel);
+        def.setEnabled(t);
+        def = (CheckBox) findViewById(R.id.kurz_vor_viertel);
+        def.setEnabled(t);
+        def = (CheckBox) findViewById(R.id.kurz_nach_halb);
+        def.setEnabled(t);
+        def = (CheckBox) findViewById(R.id.kurz_vor_halb);
+        def.setEnabled(t);
+        def = (CheckBox) findViewById(R.id.kurz_nach_dreiviertel);
+        def.setEnabled(t);
+        def = (CheckBox) findViewById(R.id.kurz_vor_dreiviertel);
+        def.setEnabled(t);
+
+    }
+
+    private void toggleHalber(Boolean t) {
+        CheckBox def = (CheckBox) findViewById(R.id.halber);
+        def.setEnabled(t);
+        EditText inp = (EditText) findViewById(R.id.halber_range);
+        inp.setEnabled(t);
+    }
+
+    public void enableAllKurz() {
+        ;
     }
 }
-/*package com.wordzoo.uhr;
-
-import android.app.Activity;
-import android.appwidget.AppWidgetManager;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.CheckBox;
-
-public class CleanClockWidgetConfigure extends Activity
-{
-	protected static final String PREFS_NAME = "it.sephiroth.apps.widget.cleanclock";
-	protected static final String PREF_PREFIX_KEY = "prefix42_";
-	protected int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
-
-	protected CheckBox mUse24Checkbox;
-	protected CheckBox mShadowCheckbox;
-
-	public CleanClockWidgetConfigure()
-	{
-		super();
-	}
-
-	@Override
-	public void onCreate( Bundle icicle )
-	{
-		super.onCreate( icicle );
-
-		setResult( RESULT_CANCELED );
-		setContentView( R.layout.configure );
-
-		mUse24Checkbox = (CheckBox) findViewById( R.id.checkbox_24 );
-		mShadowCheckbox = (CheckBox) findViewById( R.id.checkbox_shadow );
-
-		findViewById( R.id.save_button ).setOnClickListener( mOnClickListener );
-
-		Intent intent = getIntent();
-		Bundle extras = intent.getExtras();
-
-		if( extras != null )
-		{
-			mAppWidgetId = extras.getInt( AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID );
-		}
-
-		if( mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID )
-		{
-			finish();
-		}
-	}
-
-	protected View.OnClickListener mOnClickListener = new View.OnClickListener()
-	{
-
-		public void onClick( View v )
-		{
-			final Context context = getContext();
-			boolean use24 = mUse24Checkbox.isChecked();
-			boolean useShadow = mShadowCheckbox.isChecked();
-
-			savePrefs( context, use24, useShadow );
-
-			AppWidgetManager appWidgetManager = AppWidgetManager.getInstance( context );
-			Constants.log( "Configure completed!" );
-			updateAppWidget( context, use24, useShadow, appWidgetManager );
-
-			Intent resultValue = new Intent();
-			resultValue.putExtra( AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId );
-			setResult( RESULT_OK, resultValue );
-			finish();
-		}
-	};
-
-	protected Context getContext() {
-		Constants.log( this.getClass().getSimpleName() + "::getContext" );
-		return this;
-	}
-
-	protected void savePrefs( final Context context, boolean use24, boolean useShadow )
-	{
-		Constants.log( getClass().getSimpleName() + "::savePrefs" );
-		savePref( context, mAppWidgetId, Constants.PREF_24HOUR, use24 );
-		savePref( context, mAppWidgetId, Constants.PREF_SHADOW, useShadow  );
-	}
-
-	protected void updateAppWidget( final Context context, boolean use24, boolean useShadow, AppWidgetManager appWidgetManager )
-	{
-		Constants.log( getClass().getSimpleName() + "::updateAppWidget" );
-		ClockAppWidgetProvider.updateAppWidget( context, appWidgetManager, mAppWidgetId, use24, useShadow );
-	}
-
-	static void savePref( Context context, int appWidgetId, String name, Boolean value )
-	{
-		savePref( context, appWidgetId, PREF_PREFIX_KEY, name, value );
-	}
-
-	static boolean loadPref( Context context, int appWidgetId, String name )
-	{
-		return loadPref( context, appWidgetId, PREF_PREFIX_KEY, name );
-	}
-
-	protected static void savePref( Context context, int appWidgetId, String prefix, String name, Boolean value )
-	{
-		Constants.log( "savePrev. prefix=" + prefix + ", appWidgetId=" + appWidgetId + ", name=" + name );
-		SharedPreferences.Editor prefs = context.getSharedPreferences( PREFS_NAME, 0 ).edit();
-		prefs.putBoolean( prefix + appWidgetId + "-" + name, value );
-		prefs.commit();
-	}
-
-	protected static boolean loadPref( Context context, int appWidgetId, String prefix, String name )
-	{
-		Constants.log( "loadPrev. prefix=" + prefix + ", appWidgetId=" + appWidgetId + ", name=" + name );
-		SharedPreferences prefs = context.getSharedPreferences( PREFS_NAME, 0 );
-		boolean value = prefs.getBoolean( prefix + appWidgetId + "-" + name, false );
-		return value;
-	}
-
-}
-*/
