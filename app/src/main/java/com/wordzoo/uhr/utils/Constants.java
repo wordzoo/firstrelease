e7d9a729182f835409f3e46ce32c679ea1c71038
tom kast <tomkast@gmail.com>
2016-05-15 15:05:10 +0200

    bordersandstyling

--- app/src/main/java/com/wordzoo/uhr/utils/Constants.java
+++ app/src/main/java/com/wordzoo/uhr/utils/Constants.java
@@ -13,7 +13,7 @@ public class Constants {
     public final static String SETTING = "SETTING"; //note key includes clock~config~setting e.g. germanclock~myconfig~kurznach
     public final static String OFFICIAL_TIME = "Offizielle Zeit";
     public final static String INFORMAL_TIME = "Umgangssprachlich";
-    public final static String MIXED_TIME = "Gemischt";
+    public final static String MIXED_TIME = "Hybride";
     public final static String STAR_TIME = "Sterne";
 
     public final static String CONFIG_MODE_EDIT = "CONFIG_MODE_EDIT";
